5412cb6ad8df8e81068bdee8dd9b977b1fa5461f
shakib0808 <mdshakibulhasan0808@gmail.com>
2025-02-24 02:24:38 +0600

    simple iput

--- event.js
+++ event.js
@@ -4,3 +4,14 @@ function bgRed() {
 function bgBlue() {
   document.body.style.backgroundColor = "blue";
 }
+
+const button = document.getElementById('btn2')
+button.onclick = darkMode;
+
+function darkMode(){
+  document.body.style.backgroundColor= 'red'
+}
+
+document.getElementById('most').addEventListener('click',function click (){
+  document.body.style.backgroundColor= "yellow"
+})
